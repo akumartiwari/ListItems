16081da60782d431f3226ea2c658cb82455c4d3d
akumartiwari <atiwari@greenapplesolutions.com>
2019-04-12 17:14:07 +0530

    Added text box to enter data

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app">
+        <!--<img alt="Vue logo" src="./assets/logo.png">-->
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+
+//Create one more vue component and render inside DOM
+<!--<template>-->
+<!--<div id="app1">-->
+<!--<img width="25%" src="./assets/logo.png">-->
+<!--<HelloWorld/>-->
+<!--</div>-->
+<!--</template>-->
+
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
+
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <span>Multiline message is:</span>
+        <p style="white-space: pre-line;">{{ message }}</p>
+        <!--<ul>-->
+            <!--<li v-for="item in items">-->
+                <!--{{ item }}-->
+            <!--</li>-->
+        <!--</ul>-->
+        <br>
+        <textarea v-model="message" placeholder="add multiple lines"></textarea>>
+    </div>
+</template>
+
+<script>
+    export default {
+        // template: '#task-template',
+        // props: ['list'],
+        // data: function () {
+        //     return {
+        //         mutableList: JSON.parse(this.list)
+        // }
+        // },
+        name: 'HelloWorld',
+        props: {
+            msg: String,
+            message: String,
+            items: []
+        },
+
+        mounted() {
+            this.init();
+        },
+
+        async init() {
+            this.items.push(this.message);
+            setInterval(async () => {
+                this.client = await this.items.push(this.message);
+            }, 60000)
+        }
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
